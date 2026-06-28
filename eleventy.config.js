e3ccff5010866f17753ba46b402080dcc1cb363c
Martin van der Linden <115616751+MartinZwolle@users.noreply.github.com>
2026-06-28 20:55:16 +0200

    Add sitemap en robots

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,9 @@ export default function(eleventyConfig) {
   eleventyConfig.addFilter("dateToRfc822", (date) => {
     return new Date(date).toUTCString();
   });
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+  return new Date(dateObj).toISOString().split("T")[0];
+  });
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/favicon.png");
